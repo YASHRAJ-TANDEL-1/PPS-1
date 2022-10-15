8b7f66c95c2c93a937b3313f0e9760c32f9709a1
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-10-15 23:33:16 +0530

    Add files via upload

---
+++ 👾C program/SET-2/03_grestest.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the value of a =");
+    scanf("%d",&a);
+    printf("Enter the value of b =");
+    scanf("%d",&b);
+    printf("Enter the value of c =");
+    scanf("%d",&c);
+    if(a>b && a>c)
+    {
+        printf("a is the greatest");
+    }
+    else if(b>a && b>c)
+    {
+        printf("b is the greatest");
+    }
+    else
+    {
+        printf("c is the greates");
+    }
+    getch();
+}
