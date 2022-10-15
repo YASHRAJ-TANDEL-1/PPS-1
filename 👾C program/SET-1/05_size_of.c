ad11b378ff4f1ae50262708d6a4dc10b4a95a917
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-10-15 18:04:16 +0530

    Add files via upload

---
+++ 👾C program/SET-1/05_size_of.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("\n SIZE OF INTEGER = %d",sizeof(int));
+    printf("\n SIZE OF FLOAT = %d",sizeof(float));
+    printf("\n SIZE OF CHARCHTER = %d",sizeof(char));
+    printf("\n SIZE OF DOUBLE = %d",sizeof(double));
+    getch();
+}
