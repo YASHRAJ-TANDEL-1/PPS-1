44cec9c959aa0505fa2046d45d710f6c37cc96c5
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-10-22 11:53:24 +0530

    Add files via upload

---
+++ 👾C program/SET-4/02_even_odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n[10];
+    printf("Enter the number :- \n");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&n[i]);
+    }
+    getch();
+}
